8473a0be3711fdac6e7bffcd479d31052e42cb64
Brackly Murunga <khanbrackly@gmail.com>
2022-05-24 20:01:43 +0300

    Update settings.py

--- Renti/settings.py
+++ Renti/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'dmb6zk$-)#@g0p5l-h_&5yzn49ze=yuq#(5n9g^zyipj@9b75r'
+SECRET_KEY = env("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
